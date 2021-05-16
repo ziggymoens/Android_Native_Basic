18caceaf88b92a70fda2cdac545c2e5e11025c66
Ziggy Moens <ziggy.moens@endare.com>
2021-05-16 09:46:14 +0200

    Initial commit

---
+++ app/src/main/java/com/ziggymoens/androidnativebasic/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.ziggymoens.androidnativebasic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tv: TextView = findViewById(R.id.text_view)
+        tv.text = "Hello, Android ${android.os.Build.VERSION.SDK_INT}!"
+    }
+}
